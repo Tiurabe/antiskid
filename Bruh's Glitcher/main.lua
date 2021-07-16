84bd3d0553c6b55aaf73f1ce9f74eb50307574b1
Tiurabe <63129379+Tiurabe@users.noreply.github.com>
2021-07-16 19:37:45 -0300

    Add files via upload

---
+++ Bruh's Glitcher/main.lua
@@ -0,0 +1,14 @@
+-- Reanimations by Mizt, edited by SacredLua to have netless and non jitter.
+-- Glitcher by bruh
+-- THIS IS V1 SO THERE IS MORE TO COME!
+--[[CMD LIST
+.R6 -- normal R6 reanimate. Quite good.
+.FlingReanimate -- flinging R6 reanimate. Torso jitter.
+.Caster -gh 5618303165,5268602207,5207824356
+.glitcher -gh 4524991457,4820152700,4932541287,4458601937,4506945409,4315489767,4794315940
+.krystal -- nothing required
+.R6 -- nothing required
+.IDplayer -- nothing required
+.Sniper -gh 5063578607
+]] 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SampleSacred/projectxnew/main/ProjectXV1_3.lua",true))()
